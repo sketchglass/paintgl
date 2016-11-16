55f4b178950473993bc26607d42956d8f558ddd0
Ryohei Ikegami <iofg2100@gmail.com>
2016-11-17 00:09:32 +0900

    Export TextureFilter

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,6 +3,6 @@ export { Context } from "./Context";
 export { Drawable } from "./Drawable";
 export { DrawTarget, CanvasDrawTarget, TextureDrawTarget } from "./DrawTarget";
 export { Shader, ColorShader, TextureShader } from "./Shader";
-export { PixelType, PixelFormat, Texture } from "./Texture";
+export { PixelType, PixelFormat, TextureFilter, Texture } from "./Texture";
 export { Shape, QuadShape, RectShape } from "./Shape";
 export { BlendMode, Model } from "./Model";
