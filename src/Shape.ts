8c47ef85bde22da1872a6abd6ea3d3a93ee32622
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-14 16:17:31 +0900

    Optimize loop

--- src/Shape.ts
+++ src/Shape.ts
@@ -112,22 +112,23 @@ class ShapeBase {
     const stride = this.attributeStride()
     const vertexData = new Float32Array(length * stride)
 
-    for (let i = 0; i < length; ++i) {
-      let offset = 0
-      for (const name in this.attributes) {
-        const attribute = this.attributes[name]
-        if (attribute.size == 1) {
+    let offset = 0
+    for (const name in this.attributes) {
+      const attribute = this.attributes[name]
+      if (attribute.size == 1) {
+        for (let i = 0; i < length; ++i) {
           const value = attribute.data[i] as number
           vertexData[i * stride + offset] = value
-        } else {
+        }
+      } else {
+        for (let i = 0; i < length; ++i) {
           const value = attribute.data[i] as Vec2
           vertexData[i * stride + offset] = value.x
           vertexData[i * stride + offset + 1] = value.y
         }
-        offset += attribute.size
       }
+      offset += attribute.size
     }
-
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer)
     gl.bufferData(gl.ARRAY_BUFFER, vertexData, glUsage(gl, this.usage))
     gl.bindBuffer(gl.ARRAY_BUFFER, null)
