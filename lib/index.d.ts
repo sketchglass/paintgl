1ba25946b5205ea191e90a06c9ec09ee93f7609b
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-24 16:48:43 +0900

    Export UniformValue

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,3 +5,4 @@ export { Shader, colorShader, textureShader } from "./Shader";
 export { PixelType, PixelFormat, TextureFilter, Texture } from "./Texture";
 export { Shape, QuadShape, RectShape } from "./Shape";
 export { BlendMode, Model, ShapeModelMode, ShapeModel, TextureModel } from "./Model";
+export { UniformValue } from "./Program";
